c41d7365a0aa2f3a1b51ac3df1e01ed1458ed9f0
Leandro Egea <leandroe@lagash.com>
2019-05-28 17:59:50 -0300

    Casi el 56

---
+++ Ejercicios/Ejercicio Nro 56/FormNotepad.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Ejercicio_Nro_56
+{
+    partial class FormNotepad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.archivoMenuStripItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirMenuStripItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.GuardarMenuStripItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarComoMenuStripItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainStatusStrip = new System.Windows.Forms.StatusStrip();
+            this.caracteresStatusStrip = new System.Windows.Forms.ToolStripStatusLabel();
+            this.mainRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.mainMenuStrip.SuspendLayout();
+            this.mainStatusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoMenuStripItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(568, 24);
+            this.mainMenuStrip.TabIndex = 0;
+            this.mainMenuStrip.Text = "menuStrip1";
+            // 
+            // archivoMenuStripItem
+            // 
+            this.archivoMenuStripItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirMenuStripItem,
+            this.GuardarMenuStripItem,
+            this.guardarComoMenuStripItem});
+            this.archivoMenuStripItem.Name = "archivoMenuStripItem";
+            this.archivoMenuStripItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoMenuStripItem.Text = "Archivo";
+            // 
+            // abrirMenuStripItem
+            // 
+            this.abrirMenuStripItem.Name = "abrirMenuStripItem";
+            this.abrirMenuStripItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.abrirMenuStripItem.Size = new System.Drawing.Size(231, 22);
+            this.abrirMenuStripItem.Text = "Abrir";
+            this.abrirMenuStripItem.Click += new System.EventHandler(this.abrirMenuStripItem_Click);
+            // 
+            // GuardarMenuStripItem
+            // 
+            this.GuardarMenuStripItem.Name = "GuardarMenuStripItem";
+            this.GuardarMenuStripItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.GuardarMenuStripItem.Size = new System.Drawing.Size(231, 22);
+            this.GuardarMenuStripItem.Text = "Guardar";
+            // 
+            // guardarComoMenuStripItem
+            // 
+            this.guardarComoMenuStripItem.Name = "guardarComoMenuStripItem";
+            this.guardarComoMenuStripItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.guardarComoMenuStripItem.Size = new System.Drawing.Size(231, 22);
+            this.guardarComoMenuStripItem.Text = "Guardar como...";
+            this.guardarComoMenuStripItem.Click += new System.EventHandler(this.guardarComoMenuStripItem_Click);
+            // 
+            // mainStatusStrip
+            // 
+            this.mainStatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.caracteresStatusStrip});
+            this.mainStatusStrip.Location = new System.Drawing.Point(0, 313);
+            this.mainStatusStrip.Name = "mainStatusStrip";
+            this.mainStatusStrip.Size = new System.Drawing.Size(568, 22);
+            this.mainStatusStrip.TabIndex = 1;
+            this.mainStatusStrip.Text = "statusStrip1";
+            // 
+            // caracteresStatusStrip
+            // 
+            this.caracteresStatusStrip.Name = "caracteresStatusStrip";
+            this.caracteresStatusStrip.Size = new System.Drawing.Size(0, 17);
+            // 
+            // mainRichTextBox
+            // 
+            this.mainRichTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainRichTextBox.Location = new System.Drawing.Point(0, 24);
+            this.mainRichTextBox.Name = "mainRichTextBox";
+            this.mainRichTextBox.Size = new System.Drawing.Size(568, 289);
+            this.mainRichTextBox.TabIndex = 2;
+            this.mainRichTextBox.Text = "";
+            this.mainRichTextBox.TextChanged += new System.EventHandler(this.mainRichTextBox_TextChanged);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.DefaultExt = "txt";
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.Filter = "txt files (*.txt)|*.txt";
+            this.openFileDialog.InitialDirectory = "c:\\\\";
+            this.openFileDialog.RestoreDirectory = true;
+            this.openFileDialog.ShowReadOnly = true;
+            this.openFileDialog.Title = "Open";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "txt";
+            this.saveFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            this.saveFileDialog.Title = "Guardar como";
+            // 
+            // FormNotepad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 335);
+            this.Controls.Add(this.mainRichTextBox);
+            this.Controls.Add(this.mainStatusStrip);
+            this.Controls.Add(this.mainMenuStrip);
+            this.MainMenuStrip = this.mainMenuStrip;
+            this.Name = "FormNotepad";
+            this.Text = "Notepad";
+            this.Load += new System.EventHandler(this.FormNotepad_Load);
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.mainStatusStrip.ResumeLayout(false);
+            this.mainStatusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem archivoMenuStripItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirMenuStripItem;
+        private System.Windows.Forms.ToolStripMenuItem GuardarMenuStripItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarComoMenuStripItem;
+        private System.Windows.Forms.StatusStrip mainStatusStrip;
+        private System.Windows.Forms.RichTextBox mainRichTextBox;
+        private System.Windows.Forms.ToolStripStatusLabel caracteresStatusStrip;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ Ejercicios/Ejercicio Nro 56/FormNotepad.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_Nro_56
+{
+    public partial class FormNotepad : Form
+    {
+
+
+        public FormNotepad()
+        {
+            InitializeComponent();
+        }
+
+        private void FormNotepad_Load(object sender, EventArgs e)
+        {
+            mostrarCantidadCaracteres();
+        }
+
+        private void mainRichTextBox_TextChanged(object sender, EventArgs e)
+        {
+            mostrarCantidadCaracteres();
+        }
+
+        private void mostrarCantidadCaracteres()
+        {
+            caracteresStatusStrip.Text = String.Format("{0} caracteres", mainRichTextBox.Text.Length);
+        }
+
+        private void abrirMenuStripItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog.InitialDirectory = @"C:\";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //TODO me quede aca
+
+                //StreamReader archivo = new StreamReader(RutaDeArchivo);
+                //string texto = archivo.ReadToEnd();
+                //archivo.Close();
+                //return texto;
+
+                //textBox1.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void guardarComoMenuStripItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.InitialDirectory = @"C:\";
+            saveFileDialog.ShowDialog();
+        }
+    }
+}
