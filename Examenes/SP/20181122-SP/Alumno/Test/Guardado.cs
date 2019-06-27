e5dfce2836c36b4c64b3bb2d6283225f80a1cf05
Leandro Egea <leandroe@lagash.com>
2019-06-27 10:08:09 -0300

    Test Sql/Txt/Xml

---
+++ Examenes/SP/20181122-SP/Alumno/Test/Guardado.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+using Archivos;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test
+{
+    [TestClass]
+    public class Guardado
+    {
+        [TestMethod]
+        public void Test_XML()
+        {
+            //Arrange
+            Xml<Patente> xml = new Xml<Patente>();
+            Patente patentePreGuardado;
+            Patente patentePosGuardado;
+
+            //Act
+            patentePreGuardado = new Patente("AAA123", Patente.Tipo.Vieja);
+            xml.Guardar(@"C:\zzz\testpatente.xml", patentePreGuardado);
+            xml.Leer(@"C:\zzz\testpatente.xml", out patentePosGuardado);
+
+            //Assert
+            Assert.AreEqual("AAA123", patentePosGuardado.CodigoPatente);
+            Assert.AreEqual(Patente.Tipo.Vieja, patentePosGuardado.TipoCodigo);
+        }
+
+        [TestMethod]
+        public void Test_TXT()
+        {
+            //Arrange
+            Texto txt = new Texto();
+            Queue<Patente> colaPreGuardado;
+            Queue<Patente> colaPosGuardado;
+            Patente patentePosGuardado;
+
+            //Act
+            colaPreGuardado = new Queue<Patente>();
+            colaPreGuardado.Enqueue(new Patente("AAA123", Patente.Tipo.Vieja));
+            txt.Guardar(@"C:\zzz\testpatente.txt", colaPreGuardado);
+            txt.Leer(@"C:\zzz\testpatente.txt", out colaPosGuardado);
+            patentePosGuardado = colaPosGuardado.Dequeue();
+
+            //Assert
+            Assert.AreEqual("AAA123", patentePosGuardado.CodigoPatente);
+            Assert.AreEqual(Patente.Tipo.Vieja, patentePosGuardado.TipoCodigo);
+        }
+
+        //NO ESTA PROBADO EL DE SQL
+
+        [TestMethod]
+        public void Test_SQL()
+        {
+            //Arrange
+            Sql sql = new Sql();
+            Queue<Patente> colaPreGuardado;
+            Queue<Patente> colaPosGuardado;
+            Patente patentePosGuardado;
+
+            //Act
+            colaPreGuardado = new Queue<Patente>();
+            colaPreGuardado.Enqueue(new Patente("AAA123", Patente.Tipo.Vieja));
+            sql.Guardar("Patentes", colaPreGuardado);
+            sql.Leer("Patentes", out colaPosGuardado);
+            patentePosGuardado = colaPosGuardado.Dequeue();
+
+            //Assert
+            Assert.AreEqual("AAA123", patentePosGuardado.CodigoPatente);
+            Assert.AreEqual(Patente.Tipo.Vieja, patentePosGuardado.TipoCodigo);
+        }
+    }
+}
---
+++ Examenes/SP/20181122-SP/Alumno/Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("WORKGROUP")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright © WORKGROUP 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bbeb7386-f11d-463b-a08e-bf329a87f693")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
